2410937adc1703786c4cd837a6cc78c4710f5426
Luis Bizinelli <luis_bizinelli@hotmail.com>
2019-08-14 20:53:17 -0300

    inicial
    segue conforme esperado

---
+++ app/src/main/java/com/example/despesaappopet/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.despesaappopet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textDivisao, textPessoas;
+    private SeekBar seekPessoas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textPessoas = findViewById(R.id.textPessoas);
+        textDivisao = findViewById(R.id.textDivisao);
+        seekPessoas = findViewById(R.id.seekPessoas);
+
+        seekPessoas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    /*public void dividirDespesa(View view) {
+        double valor = Double.parseDouble(editValor.getText().toString());
+        double pessoas = Double.parseDouble(editPessoa.getText().toString());
+        double resultado = valor / pessoas;
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+
+        String exibir = "R$ " + df.format(resultado);
+
+        textDivisao.setText(exibir);
+    }*/
+}
